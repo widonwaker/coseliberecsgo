f1bcbb9d99afcc0858923b07abe7b10c9afd67c4
widonwaker <hey.lol@live.it>
2017-10-20 16:58:57 +0200

    Add files via upload

---
+++ js/appodeal.js
@@ -0,0 +1,10 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+function onDeviceReady() {  
+var appKey = "054627b32c7c83d2e0d0582f21d0f12e486ca5aafda6845c";
+            Appodeal.disableLocationPermissionCheck();
+            Appodeal.initialize(appKey, Appodeal.INTERSTITIAL | Appodeal.SKIPPABLE_VIDEO | Appodeal.BANNER);
+    setTimeout(
+        function() {
+          Appodeal.show(Appodeal.BANNER_BOTTOM);
+         }, 3000);
+}
---
+++ js/import_home.js
@@ -0,0 +1,34 @@
+
+
+$(document).on({
+    ajaxSend: function () { loading('show'); },
+    ajaxStart: function () { loading('show'); },
+    ajaxStop: function () { loading('hide'); },
+    ajaxError: function () { loading('hide'); }
+});
+ 
+function loading(showOrHide) {
+    setTimeout(function(){
+        $.mobile.loading(showOrHide);
+    }, 1); 
+}
+     
+var percentBar=0;	    
+            $.ajax('http://apploadin.com/FreeCSGOstuff/goal.php', 
+                {
+                    dataType: "json",
+                    jsonp: "jsoncallback",
+                    method: 'get',
+                    contentType: 'application/json',
+                    success: function (data, status) {
+                        $.each(data, function (i, item) {
+                            $('<li />')
+                            percentBar = (item.attuale * 100) / item.obiettivo;
+							 //content.appendTo(makePage);
+	                        
+                        });
+                    },
+                    error: function (xhr, d, s) {
+                        $('#output').empty().html(s);
+                    }
+                });
