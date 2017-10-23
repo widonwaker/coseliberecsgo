95a072b163368d0619da4bc3862ba99d263ce57c
widonwaker <hey.lol@live.it>
2017-10-23 14:21:26 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -18,3 +18,13 @@ document.addEventListener('deviceready', function () {
   //   Will be used to reach the user at the most optimal time of day.
   // window.plugins.OneSignal.syncHashedEmail(userEmail);
 }, false);
+
+function onDeviceReady() {  
+var appKey = "054627b32c7c83d2e0d0582f21d0f12e486ca5aafda6845c";
+            Appodeal.disableLocationPermissionCheck();
+            Appodeal.initialize(appKey, Appodeal.INTERSTITIAL | Appodeal.SKIPPABLE_VIDEO | Appodeal.BANNER);
+    setTimeout(
+        function() {
+          Appodeal.show(Appodeal.BANNER_TOP);
+         }, 3000);
+}
