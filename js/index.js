d579a049cb82afbf0a812903ce1acc0acdb431eb
widonwaker <hey.lol@live.it>
2017-10-23 15:54:45 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,13 +19,13 @@ document.addEventListener('deviceready', function () {
   // window.plugins.OneSignal.syncHashedEmail(userEmail);
 }, false);
 
-
+function onDeviceReady() {
   alert('device is ready, appodeal should initialize');
 var appKey = "054627b32c7c83d2e0d0582f21d0f12e486ca5aafda6845c";
             Appodeal.disableLocationPermissionCheck();
             Appodeal.initialize(appKey, Appodeal.INTERSTITIAL | Appodeal.SKIPPABLE_VIDEO | Appodeal.BANNER);
-    setTimeout(
-        function() {
+}
+document.addEventListener('onBannerLoaded', function(){
           Appodeal.show(Appodeal.BANNER_TOP);
-         }, 3000);
+         });
 
