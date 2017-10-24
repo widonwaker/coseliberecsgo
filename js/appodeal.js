c2794d5ddfe91ad0e50efa728ddf65d4c9a0527b
widonwaker <hey.lol@live.it>
2017-10-24 11:48:15 +0200

    Add files via upload

--- js/appodeal.js
+++ js/appodeal.js
@@ -1,7 +1,14 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-var appKey = "054627b32c7c83d2e0d0582f21d0f12e486ca5aafda6845c";
+	var devicePlatform = device.platform;
+	if (devicePlatform === "Android") {
+            var appKey = "0b9fe024c2f4c7e47d66020fa7214a0dd62d91d14e54ad45";
+	}
+	else if (devicePlatform === "iOS") {
+		    var appKey = "054627b32c7c83d2e0d0582f21d0f12e486ca5aafda6845c";
+	}
+	        alert(appKey);
             Appodeal.disableLocationPermissionCheck();
             Appodeal.confirm(Appodeal.SKIPPABLE_VIDEO);
             Appodeal.initialize(appKey, Appodeal.INTERSTITIAL | Appodeal.SKIPPABLE_VIDEO | Appodeal.NON_SKIPPABLE_VIDEO | Appodeal.BANNER);
