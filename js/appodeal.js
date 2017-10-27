3e9d5e4d043274f9042093a13d18ac26585c675a
widonwaker <hey.lol@live.it>
2017-10-27 23:47:52 +0200

    Update appodeal.js

--- js/appodeal.js
+++ js/appodeal.js
@@ -3,3 +3,9 @@
           //Appodeal.show(Appodeal.BANNER_TOP);
          }, 3000);
 
+Appodeal.cache(Appodeal.BANNER);
+Appodeal.enableBannerCallbacks(true);
+document.addEventListener('onBannerlLoaded', function(){
+  Appodeal.show(Appodeal.BANNER_TOP);
+});
+
