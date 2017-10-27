de098581bd5d1a0ee53d784f3f3dd7cb0da41bf1
widonwaker <hey.lol@live.it>
2017-10-27 13:29:10 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -18,4 +18,16 @@ document.addEventListener('deviceready', function () {
   // Sync hashed email if you have a login system or collect it.
   //   Will be used to reach the user at the most optimal time of day.
   // window.plugins.OneSignal.syncHashedEmail(userEmail);
+  
+  	var devicePlatform = device.platform;
+	if (devicePlatform === "Android") {
+            var appKey = "0b9fe024c2f4c7e47d66020fa7214a0dd62d91d14e54ad45";
+	}
+	else if (devicePlatform === "iOS") {
+		    var appKey = "054627b32c7c83d2e0d0582f21d0f12e486ca5aafda6845c";
+	}
+  alert(appKey);
+            Appodeal.disableLocationPermissionCheck();
+            Appodeal.confirm(Appodeal.SKIPPABLE_VIDEO);
+    Appodeal.initialize(appKey, Appodeal.REWARDED_VIDEO | Appodeal.INTERSTITIAL | Appodeal.SKIPPABLE_VIDEO | Appodeal.NON_SKIPPABLE_VIDEO | Appodeal.BANNER);
 }, false);
